a6474f3440cef4e4415ff206684a4472540b8f47
thdehdgns <ehdgnsdlrk@gmail.com>
2024-04-06 16:19:21 +0900

    2024 04 06 첫 Commit

---
+++ Text c# unity/Assets/Script/C#/Hello Unity.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloUnity : MonoBehaviour
+{
+    /*List<int> list = new List<int>();*/
+
+    void Start()
+    {
+        Debug.Log("Hello Unity");
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
+
